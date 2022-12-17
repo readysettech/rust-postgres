f18faecf7fbed35ca7ccfc6c184669bea5178bea
Peter Farr <Peter@PrismaPhonic.com>
2022-12-16 18:01:19 -0800

    Fix CommandComplete not writing row description
    This change ensures that CommandComplete passes back both the full tag
    (which is necessary for distinguishing select from insert, update,
    delete etc. when getting an empty return) and ensures the row
    description gets sent back as well. This is necessary for returns from
    empty SELECT statements that still require writing back the row
    description.

--- postgres-protocol/src/message/backend.rs
+++ postgres-protocol/src/message/backend.rs
@@ -435,13 +435,23 @@ impl BackendKeyDataBody {
 }
 
 pub struct CommandCompleteBody {
-    tag: Bytes,
+    pub tag: Bytes,
 }
 
 impl CommandCompleteBody {
     #[inline]
     pub fn tag(&self) -> io::Result<&str> {
         get_str(&self.tag)
+    }
+
+    #[inline]
+    pub fn tag_bytes(&self) -> &Bytes {
+        &self.tag
+    }
+
+    #[inline]
+    pub fn into_bytes(self) -> Bytes {
+        self.tag
     }
 }
 
@@ -566,10 +576,7 @@ pub struct DataRowBody {
 impl DataRowBody {
     #[inline]
     pub fn new(storage: Bytes, len: u16) -> DataRowBody {
-        Self {
-            storage,
-            len,
-        }
+        Self { storage, len }
     }
 
     #[inline]
@@ -803,6 +810,7 @@ impl ReadyForQueryBody {
     }
 }
 
+#[derive(Debug)]
 pub struct RowDescriptionBody {
     storage: Bytes,
     len: u16,
@@ -1007,6 +1015,6 @@ fn find_null(buf: &[u8], start: usize) -> io::Result<usize> {
 }
 
 #[inline]
-fn get_str(buf: &[u8]) -> io::Result<&str> {
+pub fn get_str(buf: &[u8]) -> io::Result<&str> {
     str::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))
 }
