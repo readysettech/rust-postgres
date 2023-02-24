dd368f8c7ef8e4272e917279cc52723c1fe6a090
fakeshadow <24548779@qq.com>
2023-02-24 09:56:05 +0800

    export Bytes type through DataRowBody::storage_bytes method.

--- postgres-protocol/src/message/backend.rs
+++ postgres-protocol/src/message/backend.rs
@@ -593,6 +593,11 @@ impl DataRowBody {
     pub fn buffer(&self) -> &[u8] {
         &self.storage
     }
+
+    #[inline]
+    pub fn storage_bytes(&self) -> &Bytes {
+        &self.storage
+    }
 }
 
 pub struct DataRowRanges<'a> {
