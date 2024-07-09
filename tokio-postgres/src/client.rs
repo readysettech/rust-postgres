566c75f656f2a479fb752faa0384620af4db0de9
Sidney Cammeresi <sac@readyset.io>
2024-07-09 15:57:49 -0700

    Custom patches for readyset tree
    This patch is based on tag readyset-20240711.  Previously, we
    maintained this repo as a fork and merged with upstream occasionally.
    Now we sync to upstream, discard some now unnecessary changes, and
    dump all of our custom changes into a single commit so it may be
    more easily rebased for as long as this fork may live.

--- tokio-postgres/src/client.rs
+++ tokio-postgres/src/client.rs
@@ -2,9 +2,10 @@ use crate::codec::BackendMessages;
 use crate::config::SslMode;
 use crate::connection::{Request, RequestMessages};
 use crate::copy_out::CopyOutStream;
+use crate::generic_result::GenericResult;
 #[cfg(feature = "runtime")]
 use crate::keepalive::KeepaliveConfig;
-use crate::query::RowStream;
+use crate::query::{ResultStream, RowStream};
 use crate::simple_query::SimpleQueryStream;
 #[cfg(feature = "runtime")]
 use crate::tls::MakeTlsConnect;
@@ -15,6 +16,7 @@ use crate::Socket;
 use crate::{
     copy_in, copy_out, prepare, query, simple_query, slice_iter, CancelToken, CopyInSink, Error,
     Row, SimpleQueryMessage, Statement, ToStatement, Transaction, TransactionBuilder,
+    DEFAULT_RESULT_FORMATS,
 };
 use bytes::{Buf, BytesMut};
 use fallible_iterator::FallibleIterator;
@@ -205,7 +207,7 @@ impl Client {
         }
     }
 
-    pub(crate) fn inner(&self) -> &Arc<InnerClient> {
+    pub fn inner(&self) -> &Arc<InnerClient> {
         &self.inner
     }
 
@@ -251,6 +253,28 @@ impl Client {
         T: ?Sized + ToStatement,
     {
         self.query_raw(statement, slice_iter(params))
+            .await?
+            .try_filter_map(|result| async move {
+                match result {
+                    GenericResult::Row(row) => Ok(Some(row)),
+                    GenericResult::Command(_, _) => Ok(None),
+                }
+            })
+            .try_collect()
+            .await
+    }
+
+    /// Executes a statement and returns a read or write response
+    pub async fn generic_query<T>(
+        &self,
+        statement: &T,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<Vec<GenericResult>, Error>
+    where
+        T: ?Sized + ToStatement,
+    {
+        // try_collect will wait for the ReadyForQuery message which is not strictly necessary.
+        self.generic_query_raw(statement, slice_iter(params), DEFAULT_RESULT_FORMATS)
             .await?
             .try_collect()
             .await
@@ -308,11 +332,15 @@ impl Client {
         // However, this new form with only one .await in a loop generates
         // slightly smaller codegen/stack usage for the resulting future.
         while let Some(row) = stream.try_next().await? {
-            if first.is_some() {
-                return Err(Error::row_count());
+            match row {
+                GenericResult::Row(row) => {
+                    if first.is_some() {
+                        return Err(Error::row_count());
+                    }
+                    first = Some(row);
+                }
+                GenericResult::Command(_, _) => {}
             }
-
-            first = Some(row);
         }
 
         Ok(first)
@@ -377,10 +405,20 @@ impl Client {
         query: &str,
         params: &[(&(dyn ToSql + Sync), Type)],
     ) -> Result<Vec<Row>, Error> {
-        self.query_typed_raw(query, params.iter().map(|(v, t)| (*v, t.clone())))
-            .await?
-            .try_collect()
-            .await
+        self.query_typed_raw(
+            query,
+            params.iter().map(|(v, t)| (*v, t.clone())),
+            DEFAULT_RESULT_FORMATS,
+        )
+        .await?
+        .try_filter_map(|result| async move {
+            match result {
+                GenericResult::Row(row) => Ok(Some(row)),
+                GenericResult::Command(_, _) => Ok(None),
+            }
+        })
+        .try_collect()
+        .await
     }
 
     /// The maximally flexible version of [`query_typed`].
@@ -419,12 +457,46 @@ impl Client {
     /// # Ok(())
     /// # }
     /// ```
-    pub async fn query_typed_raw<P, I>(&self, query: &str, params: I) -> Result<RowStream, Error>
+    pub async fn query_typed_raw<P, I, J>(
+        &self,
+        query: &str,
+        params: I,
+        result_formats: J,
+    ) -> Result<RowStream, Error>
     where
         P: BorrowToSql,
         I: IntoIterator<Item = (P, Type)>,
+        J: IntoIterator<Item = i16>,
     {
-        query::query_typed(&self.inner, query, params).await
+        query::query_typed(&self.inner, query, params, result_formats).await
+    }
+
+    /// Executes a generic query.
+    ///
+    /// Note that the result_formats parameter allows us to specify result formats other than the
+    /// default Some(1) (i.e. the default of using the binary format for everything) *but* this
+    /// crate is mostly written under the assumption that we only use binary format. As such,
+    /// trying to use functions like `Row::get` on the result will fail if the FromSql impl on the
+    /// result type tries to decode the result; FromSql doesn't pass in a result format to its
+    /// `from_sql` callback because it's assumed by the trait that we always use binary format. As
+    /// such, passing in non-binary formats here is only useful if we're *not* planning on decoding
+    /// the result (such as in a case where we are merely proxying the raw row to a different
+    /// client) or if we've written custom FromSql impls that expect text instead of binary format.
+    pub async fn generic_query_raw<T, P, I, J>(
+        &self,
+        statement: &T,
+        params: I,
+        result_formats: J,
+    ) -> Result<ResultStream, Error>
+    where
+        T: ?Sized + ToStatement,
+        P: BorrowToSql,
+        I: IntoIterator<Item = P>,
+        I::IntoIter: ExactSizeIterator,
+        J: IntoIterator<Item = i16>,
+    {
+        let statement = statement.__convert().into_statement(self).await?;
+        query::generic_query(&self.inner, statement, params, result_formats).await
     }
 
     /// Executes a statement, returning the number of rows modified.
@@ -510,7 +582,7 @@ impl Client {
         self.simple_query_raw(query).await?.try_collect().await
     }
 
-    pub(crate) async fn simple_query_raw(&self, query: &str) -> Result<SimpleQueryStream, Error> {
+    pub async fn simple_query_raw(&self, query: &str) -> Result<SimpleQueryStream, Error> {
         simple_query::simple_query(self.inner(), query).await
     }
 
